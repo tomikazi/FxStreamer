7d78f70b0b325033049b1743350c010c2cd0e839
Thomas Vachuska <tom@opennetworking.org>
2020-03-01 20:01:59 -0800

    Initial implementation of LedLamp MicBuddy

--- MicBuddy.ino
+++ MicBuddy.ino
@@ -4,7 +4,7 @@
 
 #define MIC_BUDDY      "MicBuddy"
 #define SW_UPDATE_URL   "http://iot.vachuska.com/MicBuddy.ino.bin"
-#define SW_VERSION      "2019.07.01.001"
+#define SW_VERSION      "2020.03.01.001"
 
 #define MIC_PIN         A0
 #define PIR_PIN          5
@@ -12,26 +12,29 @@
 WiFiUDP UDP;
 
 // TODO: Make configurable
-IPAddress dest(192,168,1,148);
+IPAddress dest(192,168,1,243);
 
-#define MAX_SAMPLES     8
 typedef struct {
-    uint16_t count;
-    uint16_t data[MAX_SAMPLES];
-} Sample;
+    uint16_t sampleavg;
+    uint16_t samplepeak;
+    uint16_t oldsample;
+} MicSample;
 
-// For moving average for the N most recent samples used to normalize the input signal.
-#define N   256
-uint32_t mat = 600 * N;
-uint16_t cutoff = 10;
+// For moving averages
+uint32_t mat = 0;
+uint32_t nmat = 0;
 
-Sample sample;
+// Sample average, max and peak detection
+uint16_t sampleavg = 0;
+uint16_t oldsample = 0;
+uint16_t samplepeak = 0;
+
+MicSample sample;
 
 void setup() {
     gizmo.beginSetup(MIC_BUDDY, SW_VERSION, "gizmo123");
     gizmo.setUpdateURL(SW_UPDATE_URL);
     gizmo.endSetup();
-    sample.count = 0;
 }
 
 void loop() {
@@ -50,20 +53,29 @@ void handleMic() {
     mat = mat + v - (mat >> 8);
     uint16_t av = abs(v - (mat >> 8));
 
-    sample.data[sample.count] = av > cutoff ? av : 0;
-    sample.count++;
-
-    if (sample.count == MAX_SAMPLES) {
-        UDP.beginPacket(dest, 7001);
-        UDP.write((char *) &sample, sizeof(sample));
-        UDP.endPacket();
-
-        for (int i = 0; i < sample.count; i++) {
-            Serial.printf("100, %d\n", sample.data[i]);
-        }
-
-        sample.count = 0;
+    if (av < 4) {
+        av = 0;
+    } else if (av > 128) {
+        av = 128;
     }
 
-    delay(10);
+    av = map(av, 0, 128, 0, 255);
+    nmat = nmat + av - (nmat >> 4);
+    sampleavg = nmat >> 4;
+
+    // We're on the down swing, so we just peaked.
+    samplepeak = av > (sampleavg + 16) && (av < oldsample);
+
+    Serial.printf("255, %d, %d, %d\n", av, sampleavg, samplepeak * 200);
+    oldsample = av;
+
+    sample.sampleavg = sampleavg;
+    sample.samplepeak = samplepeak;
+    sample.oldsample = oldsample;
+
+    UDP.beginPacket(dest, 7001);
+    UDP.write((char *) &sample, sizeof(sample));
+    UDP.endPacket();
+
+    delay(5);
 }
