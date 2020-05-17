f4cf262dac5e3f8623acf94a8b3ff2adcd959ac3
Thomas Vachuska <tom@opennetworking.org>
2020-05-17 14:45:00 -0700

    Adjusted silence detection.

--- FxStreamer.ino
+++ FxStreamer.ino
@@ -6,7 +6,7 @@
 
 #define FX_STREAMER     "FxStreamer"
 #define SW_UPDATE_URL   "http://iot.vachuska.com/FxStreamer.ino.bin"
-#define SW_VERSION      "2020.05.11.004"
+#define SW_VERSION      "2020.05.14.001"
 
 #define STATE      "/cfg/state"
 
@@ -353,8 +353,8 @@ void handleMic() {
 
         boolean wasSilenceDetected = silenceDetected;
 
-        smat = smat + av - (smat >> 10);
-        silenceDetected = (smat >> 10) < (3 * minv);
+        smat = smat + av - (smat >> 11);
+        silenceDetected = (smat >> 11) < (3 * minv);
 
         // We're on the down swing, so we just peaked.
         samplepeak = av > (sampleavg + peakdelta) && (av < oldsample);
