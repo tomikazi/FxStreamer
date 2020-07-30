30fc9d67b80637b57939637542e219318684076b
Thomas Vachuska <tom@opennetworking.org>
2020-07-29 19:55:35 -0700

    Adding last-will disconnect message.

--- FxStreamer.ino
+++ FxStreamer.ino
@@ -6,7 +6,7 @@
 
 #define FX_STREAMER     "FxStreamer"
 #define SW_UPDATE_URL   "http://iot.vachuska.com/FxStreamer.ino.bin"
-#define SW_VERSION      "2020.05.14.001"
+#define SW_VERSION      "2020.07.29.001"
 
 #define STATE      "/cfg/state"
 
